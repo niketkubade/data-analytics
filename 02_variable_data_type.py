0c01f518c9188705062e79eb1f72601b89408de2
niketkubade <112554517+niketkubade@users.noreply.github.com>
2022-08-23 04:48:47 +0530

    Initial commit

---
+++ 02_variable_data_type.py
@@ -0,0 +1,17 @@
+# numerial type
+student_count = 50      #integer value
+table_length = 12.32    #float value / decimal value
+complex_value = 2 + 1j  #complex
+
+# text type
+superhero_name = "Batman"      # string
+
+# sequence type - list, tuple, range
+# mapping type - dict
+# set type - set, frozenset
+# boolean type - True, false
+is_dark = True
+
+# Binary type - bytes, bytearray, memoryview
+# None type - NoneType
+ 
